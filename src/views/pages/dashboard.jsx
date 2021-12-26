998d0c3ea5981f7cd94307e42bea39fe9b87e622
zworo <zworo@o2.pl>
2021-12-26 11:53:17 +0100

    poc chartu

--- src/views/pages/dashboard.jsx
+++ src/views/pages/dashboard.jsx
@@ -1,7 +1,64 @@
 import React from 'react';
 import {Card, CardBody, CardTitle} from 'reactstrap';
+import faker from 'faker';
+import Chart from '../../components/chart';
 
 const DashboardPage = () => {
+    const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    const [ data, setData ] = React.useState(false);
+    React.useEffect(() => {
+        setData({
+            labels,
+            datasets: [
+                {
+                    type: 'line',
+                    label: 'Dataset 1',
+                    color: 'rgb(255, 99, 132)',
+                    yAxisID: 'left',
+                    data: [
+                        faker.datatype.number({ min: -1000, max: 1000 }),
+                        faker.datatype.number({ min: -1000, max: 1000 }),
+                        faker.datatype.number({ min: -1000, max: 1000 }),
+                        faker.datatype.number({ min: -1000, max: 1000 }),
+                        faker.datatype.number({ min: -1000, max: 1000 }),
+                        faker.datatype.number({ min: -1000, max: 1000 }),
+                        faker.datatype.number({ min: -1000, max: 1000 }),
+                    ],
+                },
+                {
+                    type: 'bar',
+                    label: 'Dataset 2',
+                    color: 'rgb(75, 192, 192)',
+                    yAxisID: 'right',
+                    data: [
+                        faker.datatype.number({ min: -1000, max: 1000 }),
+                        faker.datatype.number({ min: -1000, max: 1000 }),
+                        faker.datatype.number({ min: -1000, max: 1000 }),
+                        faker.datatype.number({ min: -1000, max: 1000 }),
+                        faker.datatype.number({ min: -1000, max: 1000 }),
+                        faker.datatype.number({ min: -1000, max: 1000 }),
+                        faker.datatype.number({ min: -1000, max: 1000 }),
+                    ],
+                },
+                {
+                    type: 'bar',
+                    label: 'Dataset 3',
+                    color: 'rgb(53, 162, 235)',
+                    yAxisID: 'right',
+                    data: [
+                        faker.datatype.number({ min: -1000, max: 1000 }),
+                        faker.datatype.number({ min: -1000, max: 1000 }),
+                        faker.datatype.number({ min: -1000, max: 1000 }),
+                        faker.datatype.number({ min: -1000, max: 1000 }),
+                        faker.datatype.number({ min: -1000, max: 1000 }),
+                        faker.datatype.number({ min: -1000, max: 1000 }),
+                        faker.datatype.number({ min: -1000, max: 1000 }),
+                    ],
+                },
+            ],
+        });
+    }, [])
+
     return (
         <div>
             <Card>
@@ -9,7 +66,7 @@ const DashboardPage = () => {
                     Masternodes
                 </CardTitle>
                 <CardBody>
-                    tresc
+                    <Chart data={data} />
                 </CardBody>
             </Card>
         </div>
