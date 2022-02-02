8823299da6be3f92bf4d20778eefddb6737156fe
Iga Slotwinska <iga.slotwinska@gmail.com>
2022-02-02 18:27:12 +0100

    XE-6 add storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,33 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: [
-    'airbnb',
-    'airbnb-typescript',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-  ],
+  extends: ["airbnb", "airbnb-typescript", "plugin:react/recommended", "plugin:react/jsx-runtime", "plugin:storybook/recommended"],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 13,
     sourceType: 'module',
-    project: ['./tsconfig.json'],
+    project: ['./tsconfig.json']
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
+  plugins: ['react', '@typescript-eslint'],
+  overrides: [
+    {
+      files: ['**/*.stories.tsx'],
+      rules: {
+        'import/no-extraneous-dependencies': 'off',
+        'import/no-anonymous-default-export': 'off',
+        'react/function-component-definition': 'off'
+      },
+    },
   ],
   rules: {
     'react/no-array-index-key': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/require-default-props': 'off',
     '@typescript-eslint/no-explicit-any': 'error',
     'import/no-relative-parent-imports': 'error'
   }
