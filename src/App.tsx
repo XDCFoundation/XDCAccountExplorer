37eb9c435e24aa1d062374f082d26c58f87d0a91
Krzysztof Kozikowski <k.kozikowski.k@gmail.com>
2022-02-08 15:37:16 +0100

    [XE-9] display toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import XdcExplorer from 'pages/xdcExplorer';
 import { BrowserRouter } from 'react-router-dom';
 import Axios from 'axios';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 Axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
@@ -8,6 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <XdcExplorer />
+      <ToastContainer />
     </BrowserRouter>
   );
 }
