c31e0bec9d8b0bfc04df64cc17530f654bdea421
Krzysztof Kozikowski <k.kozikowski.k@gmail.com>
2022-02-09 19:35:46 +0100

    [XE-9] refactor api util

--- src/domains/accounts/api.ts
+++ src/domains/accounts/api.ts
@@ -1,9 +1,10 @@
-import { get } from 'helpers/api';
+import { get, queryString } from 'helpers/api';
 import { ChartData } from 'ui/chart/types';
 import { Colors } from 'ui/chart/chart';
 import { useQuery, UseQueryResult } from 'react-query';
 import { AccountsStatsDataObject, AccountsFilters } from './types';
 
+const ACCOUNT_STATS_ENDPOINT: string = 'accountStats';
 const transposeToChartFormat = (dataObj: AccountsStatsDataObject[]): ChartData => {
   const labels: string[] = [];
   const output: ChartData = {
@@ -42,8 +43,11 @@ const transposeToChartFormat = (dataObj: AccountsStatsDataObject[]): ChartData =
   return output;
 };
 
-const getAccountsChartData = async (filters: AccountsFilters): Promise<ChartData> => get<AccountsStatsDataObject[]>('accountStats', filters)
-  .then((response) => transposeToChartFormat(response));
+const getAccountsChartData = async (filters: AccountsFilters): Promise<ChartData> => {
+  const url: string = `${ACCOUNT_STATS_ENDPOINT}?${queryString(filters)}`;
+  return get<AccountsStatsDataObject[]>(url)
+    .then((response) => transposeToChartFormat(response));
+};
 
 function useAccounts(filters: AccountsFilters): UseQueryResult<ChartData, Error> {
   return useQuery(
--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,17 +1,5 @@
 import Axios from 'axios';
 
-class FetchException extends Error {
-  code: number;
-
-  constructor(message: string, code: number) {
-    super(message);
-    this.code = code;
-
-    // Set the prototype explicitly.
-    Object.setPrototypeOf(this, FetchException.prototype);
-  }
-}
-
 const queryString = (data: { [key: string]: string; }) => {
   const ret: string[] = [];
   Object.keys(data).forEach((key) => {
@@ -21,13 +9,10 @@ const queryString = (data: { [key: string]: string; }) => {
   return ret.join('&');
 };
 
-const get = async <Type>(
-  resource: string,
-  filters: { [key: string]: string; },
-): Promise<Type> => new Promise<Type>((resolve, reject) => {
-  Axios.get(`${resource}?${queryString(filters)}`)
+const get = async <Type>(url: string): Promise<Type> => new Promise<Type>((resolve, reject) => {
+  Axios.get(url)
     .then((res) => resolve(res.data))
     .catch((err) => reject(err));
 });
 
-export { get, FetchException };
+export { get, queryString };
