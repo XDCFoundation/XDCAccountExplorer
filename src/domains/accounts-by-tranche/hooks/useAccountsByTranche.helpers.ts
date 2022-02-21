f8f7e644b1a1cc66ec04f6a7c1fd8992aea26afe
Iga Slotwinska <iga.slotwinska@gmail.com>
2022-02-21 18:56:00 +0100

    XE-19 update accountsByTranche API mocks

--- src/domains/accounts-by-tranche/hooks/useAccountsByTranche.helpers.ts
+++ src/domains/accounts-by-tranche/hooks/useAccountsByTranche.helpers.ts
@@ -1,18 +1,24 @@
 import { AccountTrancheDTO } from 'domains/accounts-by-tranche/api/accountsByTrancheApi.types';
 import { AccountTranche } from 'domains/accounts-by-tranche/accountsByTranche.types';
-import { orderBy } from 'lodash';
+import { maxBy, orderBy } from 'lodash';
 
 export function parseAccountsByTrancheDTOs(dtos: AccountTrancheDTO[]): AccountTranche[] {
-  const accountTranches = dtos.map((dto): AccountTranche => ({
-    accounts: dto.accounts,
-    accountsWithGreaterBalance: dto.accountsWithGreaterBalance,
-    balanceFrom: dto.balanceFrom,
-    balanceTo: dto.balanceTo ?? Number.POSITIVE_INFINITY,
-    usdBalance: dto.usdBalance,
-    balancePercentage: dto.balancePercentage,
-    xdcAverageBalance: dto.xdcAverageBalance,
-    xdcBalance: dto.xdcBalance,
-  }));
+  const sortedDtos = orderBy(dtos, ['balanceFrom'], ['desc']);
+  const maxXdcBalance = maxBy(sortedDtos, 'xdcBalance')?.xdcBalance;
 
-  return orderBy(accountTranches, ['balanceFrom'], ['desc']);
+  return sortedDtos.reduce((accountTranches, dto, index): AccountTranche[] => ([
+    ...accountTranches,
+    {
+      accounts: dto.accounts,
+      accountsWithGreaterBalance: index === 0
+        ? dto.accounts
+        : accountTranches[index - 1].accountsWithGreaterBalance + dto.accounts,
+      balanceFrom: dto.balanceFrom,
+      balanceTo: dto.balanceTo ?? Number.POSITIVE_INFINITY,
+      usdBalance: dto.usdBalance,
+      balancePercentage: maxXdcBalance ? dto.xdcBalance / maxXdcBalance : undefined,
+      xdcAverageBalance: dto.accounts ? dto.xdcBalance / dto.accounts : undefined,
+      xdcBalance: dto.xdcBalance,
+    },
+  ]), [] as AccountTranche[]);
 }
